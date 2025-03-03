22d2833db75cc886d1a041625fb04aabed609317
Shital Nyaupane <67561153+shitalnyaupane@users.noreply.github.com>
2025-03-03 11:45:30 +0000

    day 1 test

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "ap-south-1"  # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-00a34f4676a088a22"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
